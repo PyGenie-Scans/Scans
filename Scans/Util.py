6cb6eaf2b888dfa90513429c4633df3e51fc82c6
Adam Washington <adam.washington@stfc.ac.uk>
2017-07-18 22:22:58 +0100

    Better docstring on get_points and adding napoleon support to sphinx

--- Scans/Util.py
+++ Scans/Util.py
@@ -15,7 +15,61 @@ def get_points(
         before=None, after=None,
         **_):
     """This function takes a dictionary of keyword arguments for
-    a scan and returns the points at which the scan should be measured."""
+    a scan and returns the points at which the scan should be measured.
+
+
+    This function provides many ways to define a scan
+
+    - start point, stop point, and number of points
+    - start point, stop point, and spacing
+    - start point, spacing, and number of points
+
+    Parameters
+    ----------
+    current : float
+      The present position of the motor.  This is needed to relative scans.
+    begin : float
+      The absolute first position in the scan.  This is a valid start point.
+    end : float
+      The absolute final position in the scan.  This is a valid stop point.
+    before : float
+      The relative first position in the scan.  If the motor is currently
+      at 3 and ``before`` is set to -5, then the first scan point will be -2.
+      This is a valid start point.
+    after : float
+      The relative final position in the scan.  If the motor is currently
+      at 3 and ``before`` is set to 5, then the last scan point will be 8.
+      This is a valid stop point.
+    step : float
+      The fixed distance between points.  If the distance between the
+      beginning and end aren't an exact multiple of this step size,
+      then the end point will not be included.  This is a valid spacing.
+    stride : float
+      The approximate distance between points.  In order to ensure that
+      the ``begin`` and ``end`` points are included in the scan, a finer
+      resolution scan will be called for if the stride is not an exact
+      multiple of the distance. This is a valid spacing.
+    count : float
+      The number of measurements to perform.  A scan with a ``count`` of 2
+      would measure at only the beginning and the end.  This is a valid
+      number of points.
+    gaps : float
+      The number of steps that the motor will take.  A scan with a ``gaps``
+      of 1 would measure at only the beginning and the end.  This is a
+      valid number of points.
+
+    Returns
+    -------
+    Array of Floats
+      The positions for the scan.
+
+    Raises
+    ------
+    RuntimeError
+      If the supplied parameters cannot be combined into a coherent scan.
+
+
+    """
 
     if gaps:
         count = gaps+1
--- doc/source/conf.py
+++ doc/source/conf.py
@@ -32,6 +32,7 @@ extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.coverage',
     'sphinx.ext.mathjax',
+    'sphinxcontrib.napoleon',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
