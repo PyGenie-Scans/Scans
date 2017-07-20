084ca73c87c7f57868357ff2a9fcdbb4a7aae41a
Adam Washington <adam.washington@stfc.ac.uk>
2017-07-20 18:52:48 +0100

    Persistent multi-process plots
    I've also removed the limitation on needing to use the Gtk backend

--- Scans/multiplot.py
+++ Scans/multiplot.py
@@ -1,99 +1,112 @@
 #!/usr/bin/python2
-# pylint: skip-file
-# flake8: noqa
 """A demo of proper multiprocessing matplotlib"""
 from __future__ import print_function
-import time
 from multiprocessing import Process, Pipe
+import threading
 import numpy as np
 
-import matplotlib
-matplotlib.use('GtkAgg')
 import matplotlib.pyplot as plt
-import gobject
 
 
 class ProcessPlotter(object):
+    """
+    This object maintains a separate a separate process at the OS level
+    which manages a matplotlib plot. This is an incredibly stupid way
+    to get around the threading limitations of matplotlib, but it's
+    also the best that we've found.
+
+    Parameters
+    ----------
+
+    rehome : bool
+      If true, the graph will recenter itself every time a new point
+      is added.  Otherwise, the plot will remain where the user left
+      it, but the home functionality will not be updated after the user
+      moves the graph.
+    """
     def __init__(self, rehome=False):
         self.x = []
         self.y = []
 
         self.pipe = None
-        self.fig, self.ax = plt.subplots()
-        self.gid = gobject.timeout_add(1000, self.poll_draw())
+        self.fig = None
+        self.axis = None
         self.rehome = rehome
 
-    def terminate(self):
-        plt.close('all')
-
     def poll_draw(self):
+        """
+        Update the graph with the latest commands
+        off the process channel
+        """
 
-        def call_back():
-            while 1:
-                if not self.pipe.poll():
-                    break
+        while True:
+            if not (self.pipe and self.pipe.poll()):
+                break
 
-                command = self.pipe.recv()
-                print(command)
+            command = self.pipe.recv()
 
-                if command is None:
-                    self.terminate()
-                    print("Terminated")
-                    self.pipe.send((self.x, self.y))
-                    return False
+            if command is None:
+                self.pipe.send((self.x, self.y, self.fig))
+                return False
 
-                else:
-                    self.x.append(command[0])
-                    self.y.append(command[1])
-                    self.ax.plot(self.x, self.y, 'ro')
-                    if self.rehome:
-                        self.ax.set_xlim(min(self.x), max(self.x))
-                        self.ax.set_ylim(min(self.y), max(self.y))
+            else:
+                self.x.append(command[0])
+                self.y.append(command[1])
+                self.axis.plot(self.x, self.y, 'ro')
+                if self.rehome:
+                    self.axis.set_xlim(min(self.x), max(self.x))
+                    self.axis.set_ylim(min(self.y), max(self.y))
 
-
-            self.fig.canvas.draw()
-            return True
-
-        return call_back
+        self.fig.canvas.draw()
+        self.fig.canvas.show()
+        threading.Timer(0.5, self.poll_draw).start()
 
     def __call__(self, pipe):
-        print('starting plotter...')
 
         self.pipe = pipe
 
-        print('...done')
+        threading.Timer(0.5, self.poll_draw).start()
+        self.fig, self.axis = plt.subplots()
         plt.show()
 
 
 class NBPlot(object):
+    """
+    A non-blocking plot to get around the threading limitations of
+    matplotlib.
+    """
     def __init__(self, **kwargs):
         self.plot_pipe, plotter_pipe = Pipe()
         self.plotter = ProcessPlotter(**kwargs)
         self.plot_process = Process(target=self.plotter,
                                     args=(plotter_pipe,))
-        self.plot_process.daemon = True
+        # self.plot_process.daemon = True
         self.plot_process.start()
 
     def __call__(self, data):
         self.plot_pipe.send(data)
 
-    def __del__(self):
-        print("Deleting")
+    def join(self):
+        """Close the plot and get the results from it"""
+        if not self.plot_process.daemon:
+            return
+
         self.plot_pipe.send(None)
-        x, y = self.plot_pipe.recv()
-        print(x, y)
+        result = self.plot_pipe.recv()
         self.plot_process.join()
-        # plt.plot(x, y)
-        # plt.show()
+        return result
 
 
 def main():
-    pl = NBPlot(rehome=False)
-    boondoggle = np.arange(0, 1e7, dtype=np.uint64)
-    for ii in range(10):
-        pl([ii, ii**2])
+    """A simple test function of NBPlot"""
+    plot = NBPlot(rehome=False)
+    boondoggle = np.arange(0, 2e6, dtype=np.int64)
+    for i in range(10):
+        plot([i, i**2])
         for _ in range(30):
             boondoggle = np.sin(boondoggle**2)
+    return plot.join()
+
 
 if __name__ == '__main__':
     main()
