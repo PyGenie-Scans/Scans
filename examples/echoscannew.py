a785b43473f2778c30d95871e129ac740ae1f59d
Adam Washington <adam.washington@stfc.ac.uk>
2017-12-08 10:06:26 +0000

    Don't actually import the Larmor instrument for the echo example.

--- examples/echoscannew.py
+++ examples/echoscannew.py
@@ -2,7 +2,6 @@ if __name__ == '__main__' and __package__ is None:
     from os import sys, path
     sys.path.append(path.dirname(path.dirname(path.abspath(__file__))))
 
-from Scans import Larmor, Fit
 from Scans.Instrument import scan, THETA, pol_measure
 from Scans.Fit import DampedOscillator
 
