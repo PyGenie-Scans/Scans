a524c1163a00d75a85d82809bdfb1d384266a9d0
NDXLARMOR <adam.washington@stfc.ac.uk>
2018-05-24 15:22:57 +0100

    Add fast_pol_measure

--- Scans/Larmor.py
+++ Scans/Larmor.py
@@ -121,5 +121,33 @@ def pol_measure(**kwargs):
             pols[idx] += Polarisation(ups, down)
     return MonoidList(pols)
 
+def fast_pol_measure(**kwargs):
+    """
+    Get a single polarisation measurement
+    """
+    slices = [slice(222, 666), slice(222, 370), slice(370, 518),
+              slice(518, 666)]
+
+    i = g.get_period()
+
+    g.change(period=i+1)
+    g.waitfor_move()
+    gfrm = g.get_frames()
+    g.resume()
+    g.waitfor(frames=gfrm+kwargs["frames"])
+    g.pause()
+
+    pols = [Average.zero() for _ in slices]
+    for channel in [11, 12]:
+        mon1 = g.get_spectrum(1, i+1)
+        spec1 = g.get_spectrum(channel, i+1)
+        for idx, slc in enumerate(slices):
+            ups = Average(
+                np.sum(spec1["signal"][slc])*100.0,
+                np.sum(mon1["signal"])*100.0)
+            # print(idx, slc, ups, down, ups.err(), down.err())
+            pols[idx] += ups
+    return MonoidList(pols)
+ 
 
 scan = make_scan(Larmor())
