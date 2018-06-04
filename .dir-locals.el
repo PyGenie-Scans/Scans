df2b54448b90072203cddd6d6a51ee405e8147cb
Adam Washington <adam.washington@stfc.ac.uk>
2018-06-04 17:13:37 +0100

    Add dir-locals

---
+++ .dir-locals.el
@@ -0,0 +1,3 @@
+((nil . ((projectile-project-run-cmd . "python -m doctest doc/source/tutorial.rst")
+	 (projectile-project-compilation-cmd . "cd doc; make html")
+	 (projectile-project-test-cmd . "pylint Scans; and flake8 Scans"))))
