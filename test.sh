401b0c9deb6735bb1e0e439a35006f9918f6bb8b
Adam Washington <adam.washington@stfc.ac.uk>
2018-06-07 17:20:58 +0100

    Fix coverage badge generator

--- test.sh
+++ test.sh
@@ -3,7 +3,7 @@
 set -e
 
 coverage run --source Scans -m doctest doc/source/instrument.rst doc/source/tutorial.rst
-PERCENT=`coverage report | awk '/TOTAL/ {print $6}'`
+PERCENT=`coverage report | awk '/TOTAL/ {print $4}'`
 # coverage report | sed '/TOTAL/'
 sed -r "s/-[0-9]+%25-/-${PERCENT}25-/" readme.md > readme.md.tmp
 mv readme.md.tmp readme.md
