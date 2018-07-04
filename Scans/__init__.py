1f658a12f10e16abf7e505e6a1abc3ee1e409626
Adam Washington <adam.washington@stfc.ac.uk>
2018-07-04 12:05:33 +0100

    fix pylint warnings in init

--- Scans/__init__.py
+++ Scans/__init__.py
@@ -7,7 +7,7 @@ utilities a user could be expected to need at a beamline.
 from __future__ import absolute_import
 # We are doing some trickery with the imports that pylint doesn't
 # understand, so we need to turn off some warnings
-# pylint: disable=wildcard-import, unused-import, unused-wildcard-import, not-callable
+# pylint: disable=wildcard-import, unused-import, unused-wildcard-import
 from socket import gethostname
 from multiprocessing import current_process
 from .Fit import *  # noqa: F403,F401
@@ -18,6 +18,7 @@ host = gethostname().upper()
 
 _all = Fit.__all__[:]
 
+# pylint: disable=not-callable
 if current_process().name == "MainProcess":
     populate()
 _all += ["populate"]
